9fc144e4bf85908b0ef1134218d8e44802bca684
qubb-eizo <qubb.eq@gmail.com>
2020-06-01 18:36:25 +0300

    fix hw15

--- src/student/admin.py
+++ src/student/admin.py
@@ -5,10 +5,7 @@ from student.models import Student
 
 class StudentAdminModel(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'group')
-
-    def get_queryset(self, request):
-        return super(StudentAdminModel, self).get_queryset(request).\
-            select_related('group')
+    list_select_related = ['group']
 
 
 admin.site.register(Student, StudentAdminModel)
